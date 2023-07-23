047df412e265244eb11f8ca09de7bdc9eb824446
mcyzj <yuzijiangmn@163.com>
2023-07-23 17:57:00 +0800

    2023.7.22，添加监听事件

--- src/main/kotlin/com/dongzh1/pixelworldpro/online/online.kt
+++ src/main/kotlin/com/dongzh1/pixelworldpro/online/online.kt
@@ -63,16 +63,14 @@ object Online {
     fun verify(token:String): Boolean {
         val mac = getMacByIP()
         val dummyUrl = URL("http://zh_sh1.mcyzj.cn:1030/v1/verify/Pixelworldpro")
-        val dummyData = "token=$token&macs=$mac"
+        val data = mapOf<String, String>("token" to token, "macs" to mac)
+        val dummyData = JSONObject(data).toString()
 
         try {
             val httpUrlConnection: HttpURLConnection = dummyUrl.openConnection() as HttpURLConnection
             httpUrlConnection.requestMethod = "POST"
             httpUrlConnection.doOutput = true
             httpUrlConnection.setRequestProperty("Content-Type", "application/json")
-            httpUrlConnection.setRequestProperty("accept", "application/json")
-            httpUrlConnection.setRequestProperty("charset", "utf-8")
-            httpUrlConnection.setRequestProperty("Content-Length", dummyData.length.toString())
             val dataOutputStream = DataOutputStream(httpUrlConnection.outputStream)
             dataOutputStream.writeBytes(dummyData)
             val inputStream: InputStream = httpUrlConnection.inputStream
